7775d0a6443808d3b57a0d0c9ab635771744ed16
rachealmuchiri <rachealloveo6@gmail.com>
2025-08-04 22:37:51 +0300

    added new layout

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+// app/layout.js
+import "./globals.css";
+
+export const metadata = {
+  title: "open ressearch registry",
+  description: "A cool description of your project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-gray-900">{children}</body>
+    </html>
+  );
+}
